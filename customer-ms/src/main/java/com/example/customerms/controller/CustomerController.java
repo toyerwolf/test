41fe8cf6c6c22a5f5f7d49247efe1fe034b8647d
Huseyn Mamedov <huseynmamedov472@gmail.com>
2024-03-19 14:44:47 +0400

    Changin all

--- customer-ms/src/main/java/com/example/customerms/controller/CustomerController.java
+++ customer-ms/src/main/java/com/example/customerms/controller/CustomerController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("customers")
+@RequestMapping("customer")
 public class CustomerController {
 
     private final CustomerService customerService;
